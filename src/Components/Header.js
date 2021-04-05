6ce0118fb1abde6afbac808c4a6db9e690b447e9
amanchimania <amanchimania@gmail.com>
2021-04-05 23:13:51 +0530

    ui bugs fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,14 +6,17 @@ import Home from "./Home"
 import Login from "./Login"
 import SignUp from "./SignUp"
 import "../styles.css"
+import AboutUs from "./AboutUs";
 
 const Header = () => {
   return (
     <div className="main-header">
       <img src={logo} style={{ height: "80px", width: "80px" }}></img>&nbsp;&nbsp;&nbsp;&nbsp;
-
-      <Link to="/Home"><button>Home</button></Link>&nbsp;&nbsp;
-      <Link to="/partners"><button>Partners</button></Link>
+      <span style={{ fontSize: "xx-large" }}>Techno Hub</span>&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Link to="/Home"><button>Home</button></Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Link to="/partners"><button>Partners</button></Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <Link to="/aboutus"><button>About Us</button></Link>
       <div style={{ float: "right", position: "relative", top: "15px" }}>
         <Link to="/login"><button>Login</button></Link>
         <Link to="/Signup"><button>Signup</button></Link>
@@ -21,6 +24,7 @@ const Header = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/aboutus" component={AboutUs} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/partners" component={Partners} />
         <Route exact path="/home" component={Home} />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -35,26 +35,21 @@ export default function App() {
         <Carousel.Item>
           <img className="d-block w-100" src={c2} alt="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Windows Update </h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={c1} alt="Second slide" />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>Laptop Servicing</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={c3} alt="Third slide" />
 
           <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3>CCTV Installation</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
